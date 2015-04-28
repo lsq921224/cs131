84d2e3a040207c2ecf807a001b6a5ee9a7ff1fa0
Shengqian Liu <shengqian@Cs-212-24.CS.UCLA.EDU>
2015-04-28 15:53:17 -0700

    drf

---
+++ hw3/jmm/run_drf.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+javac BetterSorry.java
+javac BetterSafe.java
+javac UnsafeMemory.java
+echo "Synchronized"
+java UnsafeMemory Synchronized 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+echo "Unsynchronized"
+java UnsafeMemory Unsynchronized 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+echo "GetNSet"
+java UnsafeMemory GetNSet 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+echo "BetterSafe"
+java UnsafeMemory BetterSafe 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+echo "BetterSorry"
+java UnsafeMemory BetterSorry 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+rm -rf *.class
