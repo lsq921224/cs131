f2b9a59e1c0b84e18adda93e4132a1a9fce6d479
Shengqian Liu <shengqian@MacBook-Pro.local>
2015-04-28 23:50:17 -0700

    update

--- hw3/jmm/BetterSafe.java
+++ hw3/jmm/BetterSafe.java
@@ -1,46 +1,1 @@
-import java.util.concurrent.locks.ReentrantLock;
-
-class BetterSorry implements State
-{
-	private byte[] value;
-	private byte maxval;
-
-	private ReentrantLock[] locks;
-	
-	BetterSorry(byte[] v)
-	{
-		value = v;
-		maxval = 127;
-		locks = new ReentrantLock[v.length];
-		for(int i=0; i<v.length; i++) {
-			locks[i] = new ReentrantLock();
-		}	
-	}
-
-	BetterSorry(byte[] v, byte m) {
-		value = v;
-		maxval = m;
-		locks = new ReentrantLock[v.length];
-		for(int i=0; i<v.length; i++) {
-			locks[i] = new ReentrantLock();
-		}	
-	}
-
-	
-	public int size() { return value.length; }
-	
-	public byte[] current() { return value; }
-	
-	public boolean swap(int i, int j)
-	{
-		if (value[i] <= 0 || value[j] >= maxval)
-		{
-			return false;
-		}
-		if(locks[i].tryLock() && locks[j].tryLock()) {
-			value[i]--;
-			value[j]++;
-		}
-		return true;
-	}
-}
+import java.util.concurrent.locks.ReentrantLock;class BetterSorry implements State{    private byte[] value;    private byte maxval;        private ReentrantLock[] locks;        BetterSorry(byte[] v)    {        value = v;        maxval = 127;        locks = new ReentrantLock[v.length];        for(int i=0; i<v.length; i++) {            locks[i] = new ReentrantLock();        }    }        BetterSorry(byte[] v, byte m) {        value = v;        maxval = m;        locks = new ReentrantLock[v.length];        for(int i=0; i<v.length; i++) {            locks[i] = new ReentrantLock();        }    }            public int size() { return value.length; }        public byte[] current() { return value; }        public boolean swap(int i, int j)    {        if (value[i] <= 0 || value[j] >= maxval)        {            return false;        }        if(locks[i].tryLock() && locks[j].tryLock()) {            value[i]--;            value[j]++;        }        return true;    }}
--- hw3/jmm/run_drf.sh
+++ hw3/jmm/run_drf.sh
@@ -3,13 +3,13 @@ javac BetterSorry.java
 javac BetterSafe.java
 javac UnsafeMemory.java
 echo "Synchronized"
-java UnsafeMemory Synchronized 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+java UnsafeMemory Synchronized 20 10000 100 0 100 0 100 100 0 100 0 100
 echo "Unsynchronized"
-java UnsafeMemory Unsynchronized 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+java UnsafeMemory Unsynchronized 20 10000 100 0 100 0 100 100 0 100 0 100
 echo "GetNSet"
-java UnsafeMemory GetNSet 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+java UnsafeMemory GetNSet 20 10000 100 0 100 0 100 100 0 100 0 100
 echo "BetterSafe"
-java UnsafeMemory BetterSafe 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+java UnsafeMemory BetterSafe 20 10000 100 0 100 0 100 100 0 100 0 100
 echo "BetterSorry"
-java UnsafeMemory BetterSorry 20 10000000 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100 0 100
+java UnsafeMemory BetterSorry 20 100000 100 0 100 100
 rm -rf *.class
